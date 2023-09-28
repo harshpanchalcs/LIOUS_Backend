47261e8f74aec50057561083e7701d2e14d055de
Harsh Panchal <harshpanchal@microsoft.com>
2023-09-28 14:56:07 +0530

    update org

--- Controllers/organizeController.js
+++ Controllers/organizeController.js
@@ -29,9 +29,51 @@ exports.insertOrganization = (req, res) => {
 }
 
 exports.insertUnit = (req, res) => {
-    res.send('Admin Home Page');
-}
+
+    // const express = require('express');
+     const { MongoClient } = require('mongodb');
+     const app = express();
+    // const port = 3000;
+    
+    // app.use(express.json());
+    
+    const uri = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB URI
+    
+    // app.get('/checkField', async (req, res) => {
+       const Organization = req.query.Organization;
+    
+      const client = new MongoClient(uri);
+    
+      try {
+        client.connect();
+    
+        const database = client.db('LIOUS_Backend'); // Replace with your database name
+        const collection = database.collection('organizations'); // Replace with your collection name
+    
+        const filter = {};
+        filter[Organization] = { $Organization: String };
+    
+        const document =  collection.findOne(filter);
+    
+        if (document) {
+          res.json({ message: `Field "${Organization}" exists in at least one document.` });
+        } else {
+          res.json({ message: `Field "${Organization}" does not exist in any document.` });
+        }
+      } catch (error) {
+        console.error('Error checking field existence:', error);
+        res.status(500).json({ error: 'An error occurred.' });
+      }
+    }
+    
+    // app.listen(port, () => {
+    //   console.log(`Server is running on port ${port}`);
+    // });
+    
+    
+    //res.send('Insert unit');
+
 
 exports.insertEmployee = (req, res) => {
-    res.send('Admin Home Page');
+    res.send('Insert employee');
 }
